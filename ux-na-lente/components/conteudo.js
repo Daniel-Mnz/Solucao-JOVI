b966ae907709cc069dd928b1a15080a2c966ccfa
Daniel-Mnz <czpcamposlima@gmail.com>
2026-08-28 17:27:36 -0300

    Fim do projeto

--- ux-na-lente/components/conteudo.js
+++ ux-na-lente/components/conteudo.js
@@ -2,7 +2,6 @@ import Main from "./main";
 import Scanner from "./scanner";
 import Seletor from "./seletor";
 import Acessibilidade from "./acessibilidade";
-import Slideshow from "./slideshow";
 
 export default function Conteudo() {
     return (
@@ -15,8 +14,6 @@ export default function Conteudo() {
             <Seletor />
 
             <Acessibilidade />
-
-            <Slideshow />
 
         </div>
     );
